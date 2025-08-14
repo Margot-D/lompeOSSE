0fac22fcbe5b694b45addf7ce4e90bbe5c801d9e
Karl Laundal <laundal@Fjernleverbare-tjenst-utland.local>
2025-08-14 15:48:19 +0200

    added some code to calculate magnetic field coefficients

--- magnetic_field_utils/sh_analysis_lompeosse_inverse.py
+++ magnetic_field_utils/sh_analysis_lompeosse_inverse.py
@@ -8,8 +8,6 @@
 
 """
 
-# TODO get Kalle's updated file
-
 import numpy as np
 import h5py
 import dipole
@@ -76,17 +74,12 @@ def efield(x, y, Psi, returnDeltas=False, ri = 6.5*1e3):
             return (-etheta,-ephi,dtheta,dphi)  # E = -grad Psi
         else:
             return (-etheta,-ephi)  # E = -grad Psi
-        
-# --> same as my get_efield function in lompeosse
-# could be replaced by
-# from lompeosse import get_E
-# etheta, ephi = get_E()
 
 
-# datafile = '../data/msphere.mix.h5'
-datafile = '../Gamera_data.h5'
-step = 'Step#0'
-# ['Step#0', 'Step#13', 'Step#19', 'Step#20', 'Step#21']
+datafile = '../data/Gamera_data.h5'
+#datafile = '../Gamera_data.h5'
+# step = 'Step#0'
+steps = ['Step#0', 'Step#13', 'Step#19', 'Step#20', 'Step#21']
 
 data = h5py.File(datafile, 'r')
 # read coords and calculate angles
@@ -97,64 +90,64 @@ phi   = np.arctan2(y, x)
 theta = theta[:-1, :-1] + np.diff(theta, axis = 0)[:, :-1]/2
 phi   = phi[:-1, :-1] + np.diff(phi, axis = 1)[:-1, :]/2
 
-# Normalize azimuthal angles to [0 - 2pi] # !! check if useful with Kalle 
-phi[phi < 0] = phi[phi < 0] + 2*np.pi
-phi[:, 0] -= 2 * np.pi  # Adjust first column
 
-# read data
-data_step = data[step]
-Hall_n = data_step['Hall conductance NORTH'][:]
-Hall_s = data_step['Hall conductance SOUTH'][:]
-Pedersen_n = data_step['Pedersen conductance NORTH'][:]
-Pedersen_s = data_step['Pedersen conductance SOUTH'][:]
-Phi_n = data_step['Potential NORTH'][:]
-Phi_s = data_step['Potential SOUTH'][:]
+for step in steps:
+	print('doing step ' + step)
+	# read data
+	data_step = data[step]
+	Hall_n = data_step['Hall conductance NORTH'][:]
+	Hall_s = data_step['Hall conductance SOUTH'][:]
+	Pedersen_n = data_step['Pedersen conductance NORTH'][:]
+	Pedersen_s = data_step['Pedersen conductance SOUTH'][:]
+	Phi_n = data_step['Potential NORTH'][:]
+	Phi_s = data_step['Potential SOUTH'][:]
 
-# calculate the electric field
-Eth_n, Eph_n = efield(x, y, Phi_n)
-Eth_s, Eph_s = efield(x, y, Phi_s)
-Eth = np.hstack((Eth_n.flatten(), Eth_s.flatten()))
-Eph = np.hstack((Eph_n.flatten(), Eph_s.flatten()))
-Eh  = np.vstack((Eth, Eph)) # 2 x N array with theta and phi components in the rows
-
-
-# calculate the current - but first I need the main magnetic field unit vector (dipole field)
-#   and the third component of E:
-lat = np.hstack((90 - np.rad2deg(theta).flatten(), -90 + np.rad2deg(theta).flatten()))
-lon = np.hstack((np.rad2deg(phi).flatten(), np.rad2deg(phi).flatten()))
-B  = np.vstack(dp.B(lat, 1))
-b  = B / np.linalg.norm(B, axis = 0)
-b  = np.vstack((b[1], -b[0], np.zeros(b.shape[1]))) # r, theta phi
-E = np.vstack((-(b[1] * Eh[0])/b[0], Eh[0], Eh[1])) # r, theta, phi
-
-SP = np.hstack((Pedersen_n.flatten(), Pedersen_s.flatten())).reshape((1, -1)) # shape (1, N) 
-SH = np.hstack((Hall_n.flatten()    , Hall_s.flatten())    ).reshape((1, -1)) # shape (1, N)
-bxE = np.cross(b, E, axisa = 0, axisb = 0, axisc = 0)
-j = SP * E[1:] * SH * bxE[1:] # horizontal components
+	# calculate the electric field
+	Eth_n, Eph_n = efield(x, y, Phi_n)
+	Eth_s, Eph_s = efield(x, y, Phi_s)
+	Eth = np.hstack((Eth_n.flatten(), Eth_s.flatten()))
+	Eph = np.hstack((Eph_n.flatten(), Eph_s.flatten()))
+	Eh  = np.vstack((Eth, Eph)) # 2 x N array with theta and phi components in the rows
 
 
+	# calculate the current - but first I need the main magnetic field unit vector (dipole field)
+	#   and the third component of E:
+	lat = np.hstack((90 - np.rad2deg(theta).flatten(), -90 + np.rad2deg(theta).flatten()))
+	lon = np.hstack((np.rad2deg(phi).flatten(), np.rad2deg(phi).flatten()))
+	B  = np.vstack(dp.B(lat, 1))
+	b  = B / np.linalg.norm(B, axis = 0)
+	b  = np.vstack((b[1], -b[0], np.zeros(b.shape[1]))) # r, theta phi
+	E = np.vstack((-(b[1] * Eh[0])/b[0], Eh[0], Eh[1])) # r, theta, phi
 
-# spherical harmonic analysis
-N, M = 50, 50 # 150, 150 corresponds to 11475 n,m-pairs
-shbasis  = SHBasis(N, M)
-datagrid = Grid(lat = lat, lon = lon)
-datagrid_evaluator = BasisEvaluator(shbasis, datagrid, reg_lambda = 0)# 1e-5)#1e0)# 10**1)
-#gtg = datagrid_evaluator.least_squares_helmholtz.ATWA
-j_coeffs = datagrid_evaluator.grid_to_basis(j, helmholtz = True)
-j_m = datagrid_evaluator.basis_to_grid(j_coeffs, helmholtz = True)
-j_coeff_cf, j_coeff_df = j_coeffs
+	SP = np.hstack((Pedersen_n.flatten(), Pedersen_s.flatten())).reshape((1, -1)) # shape (1, N) 
+	SH = np.hstack((Hall_n.flatten()    , Hall_s.flatten())    ).reshape((1, -1)) # shape (1, N)
+	bxE = np.cross(b, E, axisa = 0, axisb = 0, axisc = 0)
+	j = SP * E[1:] * SH * bxE[1:] # horizontal components
 
-# save coefficient for each time step
-np.save(f'cfcoeff_{step}.npy', j_coeff_cf)
-np.save(f'dfcoeff_{step}.npy', j_coeff_df)
 
-# make some plots to test if it worked
-fig, ax = plt.subplots(figsize = (8, 8))
-ax.hist2d(j_m.flatten(), j.flatten(), bins = (100, 100), norm = 'log', range = ((-.3, .3), (-.3, .3)))
-ax.set_aspect('equal')
-ax.set_xlabel('j components from inversion')
-ax.set_ylabel('j components from REMIX')
-ax.plot([-.3, .3], [-.3, .3], 'k-')
-plt.show()
+
+	# spherical harmonic analysis
+	N, M = 110, 110 # 150, 150 corresponds to 11475 n,m-pairs
+	shbasis  = SHBasis(N, M)
+	datagrid = Grid(lat = lat, lon = lon)
+	datagrid_evaluator = BasisEvaluator(shbasis, datagrid, reg_lambda = 0)# 1e-5)#1e0)# 10**1)
+	#gtg = datagrid_evaluator.least_squares_helmholtz.ATWA
+	j_coeffs = datagrid_evaluator.grid_to_basis(j, helmholtz = True)
+	j_m = datagrid_evaluator.basis_to_grid(j_coeffs, helmholtz = True)
+	j_coeff_cf, j_coeff_df = j_coeffs
+
+	# save coefficient for each time step
+	np.save(f'cfcoeff_{step}.npy', j_coeff_cf)
+	np.save(f'dfcoeff_{step}.npy', j_coeff_df)
+
+	# make some plots to test if it worked
+	fig, ax = plt.subplots(figsize = (8, 8))
+	ax.hist2d(j_m.flatten(), j.flatten(), bins = (100, 100), norm = 'log', range = ((-.3, .3), (-.3, .3)))
+	ax.set_aspect('equal')
+	ax.set_xlabel(f'j components from inversion')
+	ax.set_ylabel(f'j components from REMIX')
+	ax.plot([-.3, .3], [-.3, .3], 'k-')
+	plt.savefig('magneticfield'+step.replace('#', '') + '.png', dpi = 250)
+	print('done step '+step)
 
 
--- magnetic_field_utils/sh_basis.py
+++ magnetic_field_utils/sh_basis.py
@@ -5,7 +5,7 @@ harmonic bases.
 """
 
 import numpy as np
-from .helpers import SHIndices, schmidt_normalization_factors
+from helpers import SHIndices, schmidt_normalization_factors
 
 
 class SHBasis(object):
