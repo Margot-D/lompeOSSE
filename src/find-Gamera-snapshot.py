adcb441f81ddf8277ee452a85fbc67a1ca9329ad
Margot-D <margot.decotte@gmail.com>
2026-09-12 22:02:51 +0200

    Cleaned up script for finding timestep of interest form Gamera file

--- src/find-Gamera-snapshot.py
+++ src/find-Gamera-snapshot.py
@@ -1,46 +1,77 @@
 """
-Browse individual snapshots from the provided Gamera dataset and find time step of interest 
+Browse the available snapshots in the provided Gamera dataset and inspect
+individual snapshots to find the time step of interest for a LompeOSSE experiment.
+
+Workflow:
+    1. Run this script to list all available Gamera snapshots.
+    2. Set `step` below to the snapshot you want to inspect.
+    3. The selected snapshot is loaded and plotted using Kaipy's remix module.
+
+The Gamera dataset is available from:
+https://zenodo.org/records/16882035
 """
 
+
 import kaipy.remix.remix as remix
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import h5py
 import os
 
-# Build path to Gamera data file
-path = os.path.abspath(os.path.dirname(__file__))
-datapath = os.path.join(path, 'data/Gamera_data.h5') 
-# print(datapath)
+# ---------------------------------------------------------------------------
+# Load Gamera dataset
+# ---------------------------------------------------------------------------
 
-# Check if data file exists
+path = os.path.abspath(os.path.dirname(__file__))
+datapath = os.path.join(path, 'lompeosse/data/Gamera_data.h5') 
+
 if not os.path.exists(datapath):
-    raise FileNotFoundError(
-        f"Required file not found: {datapath}\n"
-        "Please download it (https://zenodo.org/records/16882035) and place it in the 'data' folder." # TODO add zenodo link
-    )
+    raise FileNotFoundError(f"Required Gamera dataset not found: {datapath}\n"
+                            "Please download it from https://zenodo.org/records/16882035 "
+                            "and place it in the 'lompeosse/data' folder.")
+
+# ---------------------------------------------------------------------------
+# List available snapshots
+# ---------------------------------------------------------------------------
 
 with h5py.File(datapath, "r") as Gdata:
-    print("Available time steps in Gamera dataset:")
+    print("Available time steps in the provided Gamera dataset:")
+
     step_keys = [k for k in Gdata.keys() if k.startswith("Step#")]
     step_keys = sorted(step_keys, key=lambda k: int(k.split('#')[1]))    
+
     for key in step_keys:
         print("  •", key)
 
-# Select snapshot number
+# ---------------------------------------------------------------------------
+# Select a snapshot to inspect
+# ---------------------------------------------------------------------------
+
+# Change this number to the Gamera snapshot you want to inspect
 step = 12
 
-data = remix.remix(datapath,step) # use the remix portion of the <kaipy package to create the ionospheric (mix) object "data"
-data.init_vars('NORTH') # use a sub-module of the remix class to initialise the variables based on the specified hemisphere
+print(f"\nSelected snapshot: Step#{step}")
 
-# plot parameter of interest, for example:
+# ---------------------------------------------------------------------------
+# Load and plot the selected snapshot
+# ---------------------------------------------------------------------------
+
+# Use Kaipy's remix module to load the Gamera snapshot
+data = remix.remix(datapath, step)
+
+# Initialise variables for the Northern Hemisphere
+data.init_vars('NORTH')
+
+# Plot a parameter of interest
+# To see all available parameters: print(data.variables.keys())
+# For example:
+
+plt.figure()
 data.plot('current')
-print('\n snapshot #', step)
 
-# # list of the variables included in the remix-processed data object
-# keys = data.variables.keys()
-# print(keys)
-# -> ['potential', 'current', 'sigmap', 'sigmah', 'energy', 'flux', 'eflux', 
-# 'efield', 'joule', 'jhall', 'gtype', 'npsp', 'Menergy', 'Mflux', 'Meflux',
-# 'Denergy', 'Dflux', 'Deflux', 'Penergy', 'Pflux', 'Peflux']
+plt.figure()
+data.plot('potential')
 
-# to check default data limits, print data.variables
+plt.figure()
+data.plot('joule')
+
+plt.show()
