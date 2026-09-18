de01911fc754299f119fc02222e067452e623182
Margot-D <margot.decotte@gmail.com>
2026-09-18 20:05:10 +0200

    cleaned up code and improved documentation

--- src/lompeosse/visualization.py
+++ src/lompeosse/visualization.py
@@ -16,13 +16,11 @@ QUIVERSCALES = {'ground_mag':       600 * 1e-9 , # ground magnetic field scale [
                 'convection':       2000       , # convection velocity scale [m/s]
                 'efield':           100  * 1e-3, # electric field scale [V/m]
                 'electric_current': 1000 * 1e-3, # electric surface current density [A/m] Ohm's law 
-                'secs_current':     1000 * 1e-3, # electric surface current density [A/m] SECS 
-                'space_mag_full':   600 * 1e-9 } # FAC magnetic field scale [T]
+                'space_mag_full':   600 * 1e-9 } # FAC magnetic field scale TODO correct?? [T]
 
 # Default color scales (SI units):
 COLORSCALES =  {'fac':        np.linspace(-1.95, 1.95, 40) * 1e-6 * 2,
                 'ground_mag': np.linspace(-980, 980, 50) * 1e-9 / 3, # upward component
-                # 'ground_mag': np.linspace(-500, 500, 50)* 1e-9 / 3, # upward component
                 'hall':       np.linspace(0, 20, 32), # mho
                 'pedersen':   np.linspace(0, 20, 32)} # mho
 
@@ -31,41 +29,51 @@ CMAP = plt.cm.magma
 
 RE = 6371.2e3 # Earth radius in meters
 
-def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptitle=None, quiverscales=None, colorscales=None, savekw=None, clkw = {}, return_axes=False):
+def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptitle=None, quiverscales=None, colorscales=None, savekw=None, clkw = {}):
     """
     Plot Gamera simulation quantities using a Lompe-style visualization.
 
-    The plot uses visualization functions from Lompe to display
-    electrodynamics quantities extracted from a Gamera simulation on
-    the same grid as the LompeOSSE model.
+    Gamera electrodynamic quantities are extracted at the specified event time 
+    and plotted on the spatial grid defined by the LompeOSSE model. 
+    The figure follows the visualization approach used by Lompe, including maps 
+    of electric potential, convection velocity, field-aligned currents, magnetic fields,
+    conductances, and electric currents, as well as a polar view of the model domain.
 
     Parameters
     ----------
     osse_Emodel : lompe.Emodel
-        Lompe model defining the grid used for the visualization.
+        LompeOSSE model (synthetic Lompe model) defining the spatial grid.
+
     gamera_data : GameraData
-        Gamera simulation data from which the quantities are extracted.
-    ntime : int
-        Index of the Gamera simulation time step to plot.
-    
+        Gamera simulation data object providing methods for extracting electric potential, 
+        convection velocity, field-aligned currents, magnetic fields, Hall conductance, 
+        Pedersen conductance, and electric fields.
+
+    ntime : datetime-like
+        Event time used to identify the Gamera data to plot.
+
     figheight: float, optional
-        figure height - the width is determined automatically based on aspect ratios,
-        and (width, height) is the figsize given to matplotlib.pyplot.figure
-    suptitle: TODO
+        Height of the resulting figure in inches. Default is 9.
+        The figure width is determined from the aspect ratio of the model grid. 
+
+    suptitle: str, optional 
+        Title displayed at the top of the figure. Default is None.
+        
     quiverscales: dict, optional
-        dictionary of scales (in inches) to use for quiver plots. keys must be valid datatype. 
-        default values are used for datatypes that are not in list of keys
+        Custom scales for the vector plots. 
+        Values not provided use the defaults defined in ``QUIVERSCALES``.
+
     colorscales: dict, optional
-        dictionary of colorscales to use in contour plots. keys must be valid datatype. 
-        default values are used for datatypes that are not in list of keys
-    savekw: dictionary, optional
-        keyword arguments passed to savefig. If None, the figure will be shown with plt.show()
-    clkw: dictionary, optional
-        keywords for Polarplot.coastlines(), used to show coastlines in polarplot. Ignored 
-        if apex or time are not specified 
-    return_axes: bool, optional
-        Set to True to return the matplotlib figure and axes objects.
-        Default is False and will only return the matplotlib figure object
+        Custom contour levels for scalar quantities. 
+        Values not provided use the defaults defined in ``COLORSCALES``.
+
+    savekw: dict, optional
+        Keyword arguments passed to ``matplotlib.pyplot.savefig``. 
+        If ``None`` the figure is displayed with ``matplotlib.pyplot.show()``. 
+        For example, ``{'fname': 'gamera.png', 'dpi': 300}``.
+
+    clkw: dict, optional
+        Keyword arguments passed to ``Polarplot.coastlines()``.
 
     Returns
     -------
@@ -75,18 +83,19 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
     apx = apexpy.Apex(ntime.year) # apex object for magnetic coordinate calculations
 
     # ------------------------#
-    # Build plotting grid 
+    # Build evaluating/plotting grid 
     # ------------------------#
 
+    # Grid for scalar quantities
     grid = osse_Emodel.grid_J
-    lon, lat = grid.lon, grid.lat
-    xi, eta = grid.xi, grid.eta
+    slo, sla = grid.lon, grid.lat
+    sxi, seta = grid.xi, grid.eta
 
-    # grid for plotting vectors: (taken from lompe.visualization.plot_quiver)
-    sh = np.array(grid.shape) #TODO should it be grid_E?
+    # Reduced grid used for vector quantities (based on the grid used by lompe.visualization.plot_quiver)
+    sh = np.array(grid.shape)
     NN = 12 # Number of arrows to plot along smallest dimension
     sh = sh // sh.min() * NN 
-    ximin  = grid.xi .min() + grid.dxi  / 3 #TODO check that.. from quiver function
+    ximin  = grid.xi .min() + grid.dxi  / 3 
     ximax  = grid.xi .max() - grid.dxi  / 3
     etamin = grid.eta.min() + grid.deta / 3
     etamax = grid.eta.max() - grid.deta / 3
@@ -95,44 +104,41 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
 
     # ------------------------#
     # Derive the Gamera quantities 
-    # scalar are derived on osse_Emodel.grid_J, vectors in the quiver_grid
-    # TODO this is following the lompe code but check if this is how it should be done?
     # ------------------------#
 
-    # Electric potential in [V]
-    Epot = gamera_data.get_potential(lon, lat, ntime) 
+    # Electric potential
+    Epot = gamera_data.get_potential(slo, sla, ntime) # in [V]
     V = Epot - Epot.min() - (Epot.max() - Epot.min())/2 
-    V = V.reshape(grid.shape) * 1e-3 # Convert from [V] to [kV] to match lompeplot
+    V = V * 1e-3 # Convert from [V] to [kV] to match plot(potential) in lompeplot.visualization 
 
     # Convection velocity
-    Ve, Vn = gamera_data.get_V(qlo, qla, ntime)
+    Ve, Vn = gamera_data.get_V(qlo, qla, ntime) # in [m/s]
     x, y, Vx, Vy = grid.projection.vector_cube_projection(Ve, Vn, qlo, qla)
 
     # Field-aligned current (positive upward, matching Lompe)
-    facG = gamera_data.get_FAC(lon, lat, ntime)
+    facG = gamera_data.get_FAC(slo, sla, ntime) # in [A/m²]
 
     # Space magnetic field
     r_space = RE + osse_Emodel.refh*1e3 # in [m]
-    Be_space, Bn_space, Bu_space = gamera_data.get_B(qlo, qla, r=np.array(r_space), no_df_current=True, time=ntime)
+    Be_space, Bn_space, Bu_space = gamera_data.get_B(qlo, qla, r=np.array(r_space), no_df_current=True, time=ntime) # in [T]
     x, y, Bx_space, By_space = grid.projection.vector_cube_projection(Be_space, Bn_space, qlo, qla)
 
     # Ground magnetic field 
-    r_ground = RE # in [m]
+    r_ground = RE # Earth's surface in [m]
 
-    ## on quiver grid
-    Be_ground, Bn_ground, _ = gamera_data.get_B(qlo, qla, r=np.full_like(qlo, r_ground), time=ntime)
+    # Vector components evaluated on the quiver grid
+    Be_ground, Bn_ground, _ = gamera_data.get_B(qlo, qla, r=np.full_like(qlo, r_ground), time=ntime) # in [T]
     x, y, Bx_ground, By_ground = grid.projection.vector_cube_projection(Be_ground, Bn_ground, qlo, qla)
 
-    ## on OSSE model grid
-    _, _, Bu_ground = gamera_data.get_B(lon, lat, r=np.full_like(lon, r_ground), time=ntime)
-    Bu_ground = Bu_ground.reshape(grid.shape) #TODO put reshape(grid.shape) in get_B directly maybe?
+    # Upward component evaluated on the model grid for the contour plot
+    _, _, Bu_ground = gamera_data.get_B(slo, sla, r=np.full_like(slo, r_ground), time=ntime) # in [T]
 
     # Conductances
-    HallG = gamera_data.get_Hall(lon, lat, ntime)
-    PedersenG = gamera_data.get_Pedersen(lon, lat, ntime)
+    HallG = gamera_data.get_Hall(slo, sla, ntime) # in [S] = [mho]
+    PedersenG = gamera_data.get_Pedersen(slo, sla, ntime) # in [S] = [mho]
 
-    # Electric currents #TODO should it be on grid_E maybe?
-    Ee, En = gamera_data.get_E(qlo, qla, ntime)
+    # Electric currents
+    Ee, En = gamera_data.get_E(qlo, qla, ntime) # in [V/m]
     x, y, Ex, Ey = grid.projection.vector_cube_projection(Ee, En, qlo, qla)
 
     # ------------------------#
@@ -151,7 +157,7 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
         COLORSCALES.update(colorscales)
         colorscales = COLORSCALES
 
-    # potential_levels
+    # potential levels
     dV = 5 # contour level step size in kV
     potential_levels = np.r_[(V.min()//dV)*dV :(V.max()//dV)*dV + dV:dV]
 
@@ -166,7 +172,6 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
     font_scale = np.clip(area_scale, 0.8, 1.35)
 
     fig_gamera = plt.figure(figsize = figsize)
-    suptitle=f'Gamera ("truth") electrodynamics'
     fig_gamera.suptitle(suptitle, fontsize=22*font_scale, color="black", y=0.99) 
 
     row_gap = int(np.clip(0.5 + 2*(1/ar - 1), 0, 3))
@@ -177,33 +182,31 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
     for ax in axes.flatten():
         lompe.visualization.format_ax(ax, osse_Emodel, apex = apx)
 
-    # Velocity
-    # --------
-    # Convection velocity and electric potential 
+    # Convection velocity and electric potential
+    # -------- 
     ax1 = axes[0,0] 
     ax1.quiver(x, y, Vx, Vy) # convection
-    ax1.contour(xi, eta, V, colors='C0', linewidths=2, levels=potential_levels) # potential
+    ax1.contour(sxi, seta, V, colors='C0', linewidths=2, levels=potential_levels) # potential
     ax1.set_title("Convection velocity \n and \n electric potential", fontsize=15*font_scale)
 
-    # Space magnetic field
+    # FACs and space magnetic field
     # --------------------
-    # FAC and space magnetic field
     ax2 = axes[0,1]
     ax2.quiver(x, y, Bx_space, By_space, zorder=3, scale=quiverscales['space_mag_fac'], scale_units="inches")
-    ax2.contourf(xi, eta, facG, cmap='bwr', levels=colorscales['fac'], zorder=0, extend='both')
+    ax2.contourf(sxi, seta, facG, cmap='bwr', levels=colorscales['fac'], zorder=0, extend='both')
     ax2.set_title("Field-aligned currents \n and magnetic field", fontsize=15*font_scale)
 
     # Ground magnetic field
     # ---------------------
     ax3 = axes[0,2]  
     ax3.quiver(x, y, Bx_ground, By_ground, zorder=3, scale=quiverscales['ground_mag'], scale_units="inches")
-    ax3.contourf(xi, eta, Bu_ground, cmap='bwr', levels=colorscales['ground_mag'], zorder=0, extend='both')
+    ax3.contourf(sxi, seta, Bu_ground, cmap='bwr', levels=colorscales['ground_mag'], zorder=0, extend='both')
     ax3.set_title("Ground magnetic field", fontsize=15*font_scale)
 
     # Hall conductance
     # ----------------
     ax4 = axes[1,0]
-    ax4.contourf(xi, eta, HallG, cmap=CMAP, levels=colorscales['hall'], zorder=0, extend='both')
+    ax4.contourf(sxi, seta, HallG, cmap=CMAP, levels=colorscales['hall'], zorder=0, extend='both')
     lompe.visualization.plot_coastlines(ax4, osse_Emodel, color = 'grey')
     lompe.visualization.plot_mlt(ax4, osse_Emodel, ntime, apx, color = 'grey')
     ax4.set_title("Hall conductance", fontsize=15*font_scale)
@@ -211,7 +214,7 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
     # Pedersen conductance
     # --------------------
     ax5 = axes[1,1]
-    ax5.contourf(xi, eta, PedersenG, cmap=CMAP, levels=colorscales['pedersen'], zorder=0, extend='both')
+    ax5.contourf(sxi, seta, PedersenG, cmap=CMAP, levels=colorscales['pedersen'], zorder=0, extend='both')
     lompe.visualization.plot_coastlines(ax5, osse_Emodel, color = 'grey')
     lompe.visualization.plot_mlt(ax5, osse_Emodel, ntime, apx, color = 'grey')
     ax5.set_title("Pedersen conductance", fontsize=15*font_scale)
@@ -285,11 +288,11 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
     fac_levels = colorscales['fac']
     xx = np.vstack((fac_levels, fac_levels)) * 1e6
     yy = np.vstack((np.zeros_like(fac_levels), np.ones_like(fac_levels)))
-    cbarax1.contourf(xx, yy, xx, cmap = plt.cm.bwr, levels = fac_levels * 1e6)
+    cbarax1.contourf(xx, yy, xx, cmap = plt.cm.bwr, levels = fac_levels * 1e6) # in [muA/m^2]  
     cbarax1.set_xlabel(r'$\mu$A/m$^2$')
     cbarax1.set_yticks([])
     buax = plt.twiny(cbarax1)
-    buax.set_xlim(colorscales['ground_mag'].min() *1e9, colorscales['ground_mag'].max() * 1e9)
+    buax.set_xlim(colorscales['ground_mag'].min() *1e9, colorscales['ground_mag'].max() * 1e9) # in [nT]
     buax.set_xlabel('nT')
 
     conductance_levels = colorscales['hall']
@@ -301,14 +304,12 @@ def plot_gamera_lompe_style(osse_Emodel, gamera_data, ntime, figheight=9, suptit
 
     # Finish
     # ------
-    top = np.clip(0.86 - 0.06*(1/ar - 1) - 0.018*np.exp(-((ar - 1.33)/0.18)**2), 0.75, 0.91)
+    top = np.clip(0.82 - 0.06*(1/ar - 1) - 0.018*np.exp(-((ar - 1.33)/0.18)**2), 0.72, 0.91)
     plt.subplots_adjust(top=top, bottom=0.065, left=0.01, right=0.98) 
 
     if savekw != None:
         plt.savefig(**savekw)
     else:
         plt.show()
-    if return_axes==True:
-        return fig_gamera, axes, arrowax, [cbarax1, cbarax2]
-    else:
-        return fig_gamera
+
+    return fig_gamera
